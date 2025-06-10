9dd938136d7a4d3c389df01904c919089afdfd6b
Vinayak Patil <vinaykpatil24@gmail.com>
2025-06-10 22:01:16 +0530

    exporting file in nodejs

---
+++ backend/index.js
@@ -0,0 +1,3 @@
+const result = [1, 2, 3, 4];
+
+module.exports = result;
--- result.js
+++ result.js
@@ -1,3 +1,5 @@
 const result = require("./Script");
+const result2 = require("./backend");
 
 console.log(result.age);
+console.log(result2[2]);
